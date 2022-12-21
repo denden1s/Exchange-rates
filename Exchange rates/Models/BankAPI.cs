0209e58580e517d6a93739b7606cbad5044c56df
denden1s <den221001@gmail.com>
2022-12-21 15:27:42 +0300

    Create Project
    Create server and client app, complete requests to NBRB API

---
+++ Exchange rates/Models/BankAPI.cs
@@ -0,0 +1,31 @@
+﻿namespace Exchange_rates.Models
+{
+    public class BankAPI
+    {
+        private int _id;
+        public string Currency { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Value { get; set; }
+        public int Amount { get; set; }
+
+        public BankAPI()
+        { 
+
+        }
+        public BankAPI(int id)
+        {
+            _id = id;
+        }
+
+        public BankAPI(string currency, DateTime date, decimal value, int amount)
+        {
+            Currency = currency;
+            Date = date;
+            Value = value;
+            Amount = amount;
+        }
+
+    }
+
+
+}
---
+++ Exchange rates/Models/Cache.cs
@@ -0,0 +1,25 @@
+﻿namespace Exchange_rates.Models
+{
+    public class Cache
+    {
+        public BankAPI USD { get; set; }//id 431 
+        public BankAPI EUR { get; set; }//id 451
+        public BankAPI RUB { get; set; }// id 456
+        public BankAPI BTC { get; set; } // Необязательно
+
+        //сохранение кеша в файл
+
+        //как будет работать считывание кеша. 
+        //Есть файл кеша, если там нет данных по текущему периоду, то добавить их туда, если есть то обращаться
+        //к файлу а не апи.
+
+        //запрос на динамический период:
+        //https://www.nbrb.by/api/exrates/rates/dynamics/{id}?startdate={2022-5-5}&enddate={date}
+
+        //Единичный запрос данных
+        // 
+        //проверка дат должна быть по объектам конкретной валюты linq запросы
+        //для проверки на отсутствие можно использовать разность множеств
+    }
+
+}
